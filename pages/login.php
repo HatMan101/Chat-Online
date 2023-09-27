fb13ae4b5068008313d141f3f3aedb31393b80d7
VB-69 <victor.bortner@elev.ga.lbs.se>
2023-09-27 10:30:45 +0200

    first commit

---
+++ pages/login.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Sign-In</title>
+    <?php require_once "components/head.html"; ?>
+</head>
+<body>
+    <?php require_once "dbFiles/loginDB.php"?>
+    <?php require_once "components/header.html"; ?>
+    <main>
+        <h2>Sign-In</h2>
+
+    </main>
+    <?php include_once "components/footer.html"; ?>
+</body>
+</html>
